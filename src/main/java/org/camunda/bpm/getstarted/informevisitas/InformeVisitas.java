b142916f21f527a8a568125ac6fb812a2ba5ce56
eduardo7medel <emedel@appwhere.mx>
2018-07-20 11:20:30 -0500

    first commit

---
+++ src/main/java/org/camunda/bpm/getstarted/informevisitas/InformeVisitas.java
@@ -0,0 +1,14 @@
+package org.camunda.bpm.getstarted.informevisitas;
+
+
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+
+
+@ProcessApplication("Cartografia Minera")
+
+public class InformeVisitas extends ServletProcessApplication {
+
+}
